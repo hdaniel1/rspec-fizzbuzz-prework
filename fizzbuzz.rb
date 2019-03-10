d4c46537fbb53aae2e91b670d1e37035ae2c3fa8
Daniel Hallada <hallada.daniel@gmail.com>
2019-03-10 18:26:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(num1)
-  if num1 % 3 == 0
-    return "Fizz"
+  if num1 % 3 == 0 && num1 % 5 == 0
+    return "FizzBuzz"
   elsif num1 % 5 == 0
     return "Buzz"
-  elsif (num1 % 3 == 0 && num1 % 5 == 0)
-    return "FizzBuzz"
+  elsif num1 % 3 == 0
+    return "Fizz"
   else
     return nil
   end
