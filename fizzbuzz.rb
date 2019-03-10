c850fb1364674ede3e7ae57bbd3035b50799f272
Daniel Hallada <hallada.daniel@gmail.com>
2019-03-10 18:22:23 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,11 @@
-def fizzbuzz(num)
-
+def fizzbuzz(num1)
+  if num1 % 3 == 0
+    return "Fizz"
+  elsif num1 % 5 == 0
+    return "Buzz"
+  elsif num1 % 3 == 0 && num1 % 5 == 0
+    return "FizzBuzz"
+  else 
+    return nil
+  end
 end
